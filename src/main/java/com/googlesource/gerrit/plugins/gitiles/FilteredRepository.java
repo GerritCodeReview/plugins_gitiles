2a06e6daddba296ae708b2490a7e27180580c74b
Dave Borowitz <dborowitz@google.com>
2013-03-27 00:48:02 -0400

    Initial implementation of a Gitiles plugin for Gerrit
    Runs Gitiles at /plugins/gitiles/, configured to list and access
    repositories using Gerrit access controls.
    
    Change-Id: Iba5bb8c457f1ebf35ee1bc14e06f724bb021d366

---
+++ src/main/java/com/googlesource/gerrit/plugins/gitiles/FilteredRepository.java
@@ -0,0 +1,218 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.gitiles;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.git.ChangeCache;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.gerrit.server.git.TagCache;
+import com.google.gerrit.server.git.VisibleRefFilter;
+import com.google.gerrit.server.project.NoSuchProjectException;
+import com.google.gerrit.server.project.ProjectControl;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+import org.eclipse.jgit.internal.storage.file.ReflogReader;
+import org.eclipse.jgit.lib.ObjectDatabase;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.RefDatabase;
+import org.eclipse.jgit.lib.RefRename;
+import org.eclipse.jgit.lib.RefUpdate;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.RepositoryBuilder;
+import org.eclipse.jgit.lib.StoredConfig;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+class FilteredRepository extends Repository {
+  static class Factory {
+    private final Provider<ReviewDb> db;
+    private final ProjectControl.GenericFactory projectControlFactory;
+    private final Provider<CurrentUser> userProvider;
+    private final GitRepositoryManager repoManager;
+    private final TagCache tagCache;
+    private final ChangeCache changeCache;
+
+    @Inject
+    Factory(Provider<ReviewDb> db,
+        ProjectControl.GenericFactory projectControlFactory,
+        Provider<CurrentUser> userProvider,
+        GitRepositoryManager repoManager,
+        TagCache tagCache,
+        ChangeCache changeCache) {
+      this.db = db;
+      this.projectControlFactory = projectControlFactory;
+      this.userProvider = userProvider;
+      this.repoManager = repoManager;
+      this.tagCache = tagCache;
+      this.changeCache = changeCache;
+    }
+
+    FilteredRepository create(Project.NameKey name)
+        throws NoSuchProjectException, IOException {
+      ProjectControl ctl = projectControlFactory.controlFor(name, userProvider.get());
+      if (!ctl.isVisible()) {
+        throw new NoSuchProjectException(name);
+      }
+      Repository repo = repoManager.openRepository(name);
+      return new FilteredRepository(ctl, repo,
+          new VisibleRefFilter(tagCache, changeCache, repo, ctl, db.get(), true));
+    }
+  }
+
+  private final Repository delegate;
+  private final RefDatabase refdb;
+
+  private FilteredRepository(ProjectControl ctl, Repository delegate,
+      VisibleRefFilter refFilter) {
+    super(toBuilder(delegate));
+    this.delegate = delegate;
+    if (ctl.allRefsAreVisible()) {
+      this.refdb = delegate.getRefDatabase();
+    } else {
+      this.refdb = new FilteredRefDatabase(delegate.getRefDatabase(), refFilter);
+    }
+  }
+
+  private static RepositoryBuilder toBuilder(Repository repo) {
+    RepositoryBuilder b = new RepositoryBuilder().setGitDir(repo.getDirectory())
+      .setFS(repo.getFS());
+    if (!repo.isBare()) {
+      b.setWorkTree(repo.getWorkTree()).setIndexFile(repo.getIndexFile());
+    }
+    return b;
+  }
+
+  @Override
+  public void create(boolean bare) throws IOException {
+    throw new UnsupportedOperationException(); // Gitiles is read-only.
+  }
+
+  @Override
+  public ObjectDatabase getObjectDatabase() {
+    // No access control on object database; Gitiles ensures objects are only
+    // accessed if they are visible by the user according to ACLs on the refs.
+    return delegate.getObjectDatabase();
+  }
+
+  @Override
+  public RefDatabase getRefDatabase() {
+    return refdb;
+  }
+
+  @Override
+  public StoredConfig getConfig() {
+    return delegate.getConfig();
+  }
+
+  @Override
+  public void scanForRepoChanges() throws IOException {
+    throw new UnsupportedOperationException(); // Gitiles is read-only.
+  }
+
+  @Override
+  public void notifyIndexChanged() {
+    throw new UnsupportedOperationException(); // Gitiles is read-only.
+  }
+
+  @Override
+  public ReflogReader getReflogReader(String refName) throws IOException {
+    return getRef(refName) != null ? delegate.getReflogReader(refName) : null;
+  }
+
+  @Override
+  public void close() {
+    refdb.close();
+    delegate.close();
+  }
+
+  private static class FilteredRefDatabase extends RefDatabase {
+    private final RefDatabase delegate;
+    private final VisibleRefFilter refFilter;
+
+    private FilteredRefDatabase(RefDatabase delegate, VisibleRefFilter refFilter) {
+      this.delegate = delegate;
+      this.refFilter = refFilter;
+    }
+
+    @Override
+    public void create() throws IOException {
+      throw new UnsupportedOperationException(); // Gitiles is read-only.
+    }
+
+    @Override
+    public void close() {
+      // Closed by FilteredRepository.
+    }
+
+    @Override
+    public boolean isNameConflicting(String name) throws IOException {
+      return delegate.isNameConflicting(name);
+    }
+
+    @Override
+    public RefUpdate newUpdate(String name, boolean detach) throws IOException {
+      throw new UnsupportedOperationException(); // Gitiles is read-only.
+    }
+
+    @Override
+    public RefRename newRename(String fromName, String toName)
+        throws IOException {
+      throw new UnsupportedOperationException(); // Gitiles is read-only.
+    }
+
+    @Override
+    public Ref getRef(String name) throws IOException {
+      Ref ref = delegate.getRef(name);
+      if (ref == null) {
+        return null;
+      }
+      return refFilter.filter(ImmutableMap.of(ref.getName(), ref), true).get(ref.getName());
+    }
+
+    @Override
+    public Map<String, Ref> getRefs(String prefix) throws IOException {
+      Map<String, Ref> refs = delegate.getRefs(prefix);
+      Map<String, Ref> result = Maps.newHashMapWithExpectedSize(refs.size());
+      for (Ref ref : refs.values()) {
+        // VisibleRefFilter adds the prefix to the keys, re-strip it.
+        result.put(ref.getName().substring(prefix.length()), ref);
+      }
+      return refs;
+    }
+
+    @Override
+    public List<Ref> getAdditionalRefs() throws IOException {
+      List<Ref> refs = delegate.getAdditionalRefs();
+      Map<String, Ref> result = Maps.newHashMapWithExpectedSize(refs.size());
+      for (Ref ref : refs) {
+        result.put(ref.getName(), ref);
+      }
+      return ImmutableList.copyOf(result.values());
+    }
+
+    @Override
+    public Ref peel(Ref ref) throws IOException {
+      return delegate.peel(ref);
+    }
+  }
+}
---
+++ src/main/java/com/googlesource/gerrit/plugins/gitiles/Module.java
@@ -0,0 +1,101 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.gitiles;
+
+import com.google.gerrit.server.config.AllProjectsName;
+import com.google.gerrit.server.config.AllProjectsNameProvider;
+import com.google.gerrit.server.config.CanonicalWebUrl;
+import com.google.gerrit.server.config.GerritServerConfig;
+import com.google.gerrit.server.ssh.SshAdvertisedAddresses;
+import com.google.gitiles.DefaultUrls;
+import com.google.gitiles.GitilesAccess;
+import com.google.gitiles.GitilesUrls;
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.ProvisionException;
+import com.google.inject.TypeLiteral;
+
+import org.eclipse.jgit.lib.Config;
+import org.eclipse.jgit.transport.resolver.RepositoryResolver;
+
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.List;
+
+import javax.annotation.Nullable;
+import javax.servlet.http.HttpServletRequest;
+
+class Module extends AbstractModule {
+  @Override
+  protected void configure() {
+    bind(GitilesAccess.Factory.class).to(GerritGitilesAccess.Factory.class);
+    bind(new TypeLiteral<RepositoryResolver<HttpServletRequest>>() {}).to(Resolver.class);
+    bind(AllProjectsName.class).toProvider(AllProjectsNameProvider.class);
+  }
+
+  @Provides
+  GitilesUrls getGitilesUrls(@GerritServerConfig Config gerritConfig,
+      @Nullable @CanonicalWebUrl String gerritUrl,
+      @SshAdvertisedAddresses List<String> advertisedSshAddresses)
+      throws MalformedURLException, UnknownHostException {
+    URL u;
+    String hostName;
+    if (gerritUrl != null) {
+      u = new URL(gerritUrl);
+      hostName = u.getHost() != null ? u.getHost() : getLocalHostName();
+    } else {
+      u = null;
+      hostName = "Gerrit";
+    }
+
+    // Arbitrarily prefer SSH, then HTTP, then git.
+    // TODO: Use user preferences.
+    String gitUrl;
+    if (!advertisedSshAddresses.isEmpty()) {
+      String addr = advertisedSshAddresses.get(0);
+      if (addr.startsWith("*:") || "".equals(addr)) {
+        if (u != null && u.getHost() != null) {
+          // Roughly matches Gerrit.initHostname();
+          addr = u.getHost();
+          int d1 = addr.indexOf('.');
+          if (d1 >= 0) {
+            int d2 = addr.indexOf('.', d1 + 1);
+            if (d2 >= 0) {
+              addr = addr.substring(0, d2);
+            }
+          }
+        } else {
+          addr = getLocalHostName();
+        }
+      }
+      gitUrl = "ssh://" + addr + "/";
+    } else {
+      gitUrl = gerritConfig.getString("gerrit", null, "gitHttpUrl");
+      if (gitUrl == null) {
+        gitUrl = gerritConfig.getString("gerrit", null, "canonicalGitUrl");
+      }
+    }
+    if (gitUrl == null) {
+      throw new ProvisionException("Unable to determine any canonical git URL from gerrit.config");
+    }
+    return new DefaultUrls(hostName, gitUrl, gerritUrl);
+  }
+
+  private String getLocalHostName() throws UnknownHostException {
+    return InetAddress.getLocalHost().getCanonicalHostName();
+  }
+}
